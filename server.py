8841130fc7627d94eeaf0d0bab0c583e4fcd6a8e
Ruben Sanduleac <ruvim.sanduleac@gmail.com>
2022-03-12 11:09:43 -0800

    started on the personal uv and set up the flask server

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def main_page():
-    return '<h1>Hello World</h>'
+def home():
+    return render_template("ruben.html")
 
 
 if __name__ == '__main__':
